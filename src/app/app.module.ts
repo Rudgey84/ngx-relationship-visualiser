7b12e84c2007391fc73e17a3ae777dcbb147e579
Rudgey84 <me@tomrudge.com>
2023-04-20 13:55:14 +0100

    1 file modified, 1 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { DirectedGraphExperimentComponent } from './directed-graph-experiment.component';
-import { ZoomableDirective } from './visualiser/directives/zoomable.directive';
 import { AppComponent } from './app.component';
 import { ContextMenuModule } from 'ngx-contextmenu';
 import { ContextMenusComponent } from './visualiser/context-menus/context-menus.component';
@@ -11,7 +10,6 @@ import { ModalModule } from 'ngx-bootstrap/modal';
   declarations: [
     AppComponent,
     DirectedGraphExperimentComponent,
-    ZoomableDirective,
     ContextMenusComponent
   ],
   imports: [ContextMenuModule.forRoot({useBootstrap4: true}), BrowserModule, ModalModule.forRoot(),],
