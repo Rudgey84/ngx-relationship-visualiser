6021e25afa7fa0d2be8a03a5580702dd7d768559
Rudgey84 <tom@rudge.email>
2024-12-02 14:47:56 +0000

    Updated to Angular 16

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,18 @@
-import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { NgModule} from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { ContextMenuModule } from '@perfectmemory/ngx-contextmenu';
 import { ModalModule } from 'ngx-bootstrap/modal';
 import { AppComponent } from './app.component';
 import { VisualiserGraphComponent } from './visualiser-graph.component';
-import { ContextMenusComponent } from './visualiser/context-menus/context-menus.component';
-import { ContextMenuService } from '@perfectmemory/ngx-contextmenu';
 
 @NgModule({
   declarations: [
     AppComponent,
-    VisualiserGraphComponent,
-    ContextMenusComponent
+    VisualiserGraphComponent
   ],
   imports: [
     BrowserModule,
-    ContextMenuModule,
     ModalModule.forRoot()
   ],
-  providers: [ContextMenuService],
-  bootstrap: [AppComponent],
-  schemas: [CUSTOM_ELEMENTS_SCHEMA]
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
