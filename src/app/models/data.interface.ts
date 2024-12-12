bcc1bd52abae543b33c0a08ebd64b9fb64c672e1
Tom Rudge <me@tomrudge.com>
2024-12-12 12:57:42 +0000

    Added interface and extracted mock data

---
+++ src/app/models/data.interface.ts
@@ -0,0 +1,46 @@
+// data.interface.ts
+
+export interface Relationship {
+    label: string | string[];
+    lineStyle: string;
+    source: string;
+    sourceArrow: boolean;
+    target: string;
+    targetArrow: boolean;
+    attachedToAuthorisedIRs: boolean;
+    attachedToUnauthorisedIRs: boolean;
+  }
+  
+  export interface Link {
+    source: string;
+    target: string;
+    label: string[];
+    lineStyle: string;
+    sourceArrow: boolean;
+    targetArrow: boolean;
+    linkId: string;
+    relationships: Relationship[];
+  }
+  
+  export interface Node {
+    id: string;
+    version: number;
+    typeName: string;
+    label: string[];
+    icon: string;
+    xpos: number;
+    ypos: number;
+    x: number;
+    y: number;
+    fx: number | null;
+    fy: number | null;
+    attachedToAuthorisedIRs: boolean;
+    attachedToUnauthorisedIRs: boolean;
+  }
+  
+  export interface Data {
+    irUrn: string;
+    nodes: Node[];
+    links: Link[];
+  }
+  
