92efa0a7bfbae5c02eab16b8938ea39366ec8348
Tom Rudge <me@tomrudge.com>
2024-12-29 12:07:23 +0000

    Refactor application module to use NgxRelationshipVisualiserModule and move related components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,29 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { ReactiveFormsModule } from '@angular/forms';
-import { ModalModule } from 'ngx-bootstrap/modal';
-import { ContextMenuModule } from '@kreash/ngx-contextmenu';
 import { AppComponent } from './app.component';
-import { VisualiserGraphComponent } from './ngx-relationship-visualiser/visualiser/visualiser-graph/visualiser-graph.component';
-import { ContextMenusComponent } from './ngx-relationship-visualiser/visualiser/context-menus/context-menus.component';
-import { ModalsComponent } from './ngx-relationship-visualiser/visualiser/modals/modals.component';
-import { NgSelectModule } from '@ng-select/ng-select';
-import { FormsModule } from '@angular/forms';
+import { NgxRelationshipVisualiserModule } from './ngx-relationship-visualiser/ngx-relationship-visualiser.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    VisualiserGraphComponent,
-    ContextMenusComponent,
-    ModalsComponent
   ],
   imports: [
     BrowserModule,
-    ReactiveFormsModule,
-    ModalModule.forRoot(),
-    ContextMenuModule.forRoot({ useBootstrap4: true }),
-    NgSelectModule,
-    FormsModule
+    NgxRelationshipVisualiserModule
   ],
   bootstrap: [AppComponent]
 })
---
+++ src/app/ngx-relationship-visualiser/ngx-relationship-visualiser.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
+import { ModalModule } from 'ngx-bootstrap/modal';
+import { ContextMenuModule } from '@kreash/ngx-contextmenu';
+import { VisualiserGraphComponent } from '../ngx-relationship-visualiser/visualiser/visualiser-graph/visualiser-graph.component';
+import { ContextMenusComponent } from '../ngx-relationship-visualiser/visualiser/context-menus/context-menus.component';
+import { ModalsComponent } from '../ngx-relationship-visualiser/visualiser/modals/modals.component';
+import { NgSelectModule } from '@ng-select/ng-select';
+import { FormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [
+    VisualiserGraphComponent,
+    ContextMenusComponent,
+    ModalsComponent
+  ],
+  imports: [
+    BrowserModule,
+    ReactiveFormsModule,
+    ModalModule.forRoot(),
+    ContextMenuModule.forRoot({ useBootstrap4: true }),
+    NgSelectModule,
+    FormsModule
+  ],
+  exports: [VisualiserGraphComponent]
+})
+
+export class NgxRelationshipVisualiserModule { }
